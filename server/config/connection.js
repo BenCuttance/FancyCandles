8d0349289cef9b9cbe3655c3abe91d87ec9e25b8
Robinson Dos Santos <robinsonfdossantos@gmail.com>
2023-08-24 19:59:45 +0930

    Pushing to Heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,20 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
+const dbURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping';
 
-module.exports = mongoose.connection;
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const dbConnection = mongoose.connection;
+
+dbConnection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+dbConnection.on('error', (error) => {
+  console.error('MongoDB connection error:', error);
+});
+
+module.exports = dbConnection;
